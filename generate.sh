46137feb4236fd6a0d61a9d15b2f54a8a35f211b
gkoren <gkoren@nvidia.com>
2024-07-25 02:52:59 -0700

    adding generate script

---
+++ generate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python generate/generate.py --prompts prompts/generation-prompts.json --model codellama/CodeLlama-7b-hf --output outputs/output.json --max_new_tokens 1024 --num_samples_per_prompt 5 --temperature 0.2 --top_p 0.95 --prompted --restart --batch_size 16 --do_sample
